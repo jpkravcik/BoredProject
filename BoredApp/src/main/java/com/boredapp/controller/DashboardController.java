b0eca0257d87d92198c24596254d05bde724c41b
marijajov65 <42313585+marijajov65@users.noreply.github.com>
2021-11-06 18:42:53 -0500

    added filters for category

--- BoredApp/src/main/java/com/boredapp/controller/DashboardController.java
+++ BoredApp/src/main/java/com/boredapp/controller/DashboardController.java
@@ -8,9 +8,12 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 
 import com.boredapp.model.Activity;
+import com.boredapp.model.ActivityInCategory;
+import com.boredapp.model.Category;
 import com.boredapp.model.Incategory;
 import com.boredapp.model.User;
 import com.boredapp.repository.ActivityRepository;
+import com.boredapp.repository.CategoryRepository;
 import com.boredapp.repository.IncategoryRepository;
 import com.boredapp.repository.UserRepository;
 import com.boredapp.service.UserService;
@@ -26,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @RequestMapping("userhomepage")
-@SessionAttributes({"user"})
+@SessionAttributes({"user", "category", "cost"})
 @Controller
 public class DashboardController {
     
@@ -39,37 +42,104 @@ public class DashboardController {
     @Autowired
     IncategoryRepository incategoryRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+
+
+    @GetMapping("/cost/{cost}/category/{category}")
+    public ModelAndView filter(@PathVariable (name="cost") Integer cost,  @PathVariable (name="category") String category, Model model){
+       ModelAndView mv = new ModelAndView("userhomepage");
+       ArrayList<Activity> activities= (ArrayList<Activity>) activityRepository.findAll();
+
+       int index = (int)(Math.random() * activities.size());
+    mv.addObject("randomActivity", activities.get(index));
+
+    ArrayList<ActivityInCategory> activityInCategories = initializeActivityInCategory();
+    activityInCategories.removeIf(activity->(activity.getCost()>cost));
+    if(!category.equals("ALL") && !activityInCategories.isEmpty()){
+        activityInCategories.removeIf(activity->(!activity.getCategory().equals(category)));
+    }
+
+    mv.addObject("cost", cost);
+    mv.addObject("category", category);
+
+    User user = (User) model.asMap().get("user");
+    mv.addObject("user", user);      
+    mv.addObject("activities", activityInCategories);
+    return mv;
+    }
+
+
+/*
     
     @GetMapping("/cost/{value}")
     public ModelAndView filterByCost(@PathVariable (name="value") Integer value, Model model){
-        ModelAndView mv = new ModelAndView("userhomepage");
-       ArrayList<Activity> activities=(ArrayList<Activity>) activityRepository.findAll();
-       int index = (int)(Math.random() * activities.size());
-        mv.addObject("randomActivity", activities.get(index));
+       ModelAndView mv = new ModelAndView("userhomepage");
+       ArrayList<Activity> activities= (ArrayList<Activity>) activityRepository.findAll();
 
-    activities.removeIf(activity->(activity.getCost()>value));
+       int index = (int)(Math.random() * activities.size());
+    mv.addObject("randomActivity", activities.get(index));
+
+    ArrayList<ActivityInCategory> activityInCategories = initializeActivityInCategory();
+    activityInCategories.removeIf(activity->(activity.getCost()>value));
     User user = (User) model.asMap().get("user");
     mv.addObject("user", user);      
-    mv.addObject("activities", activities);
+    mv.addObject("activities", activityInCategories);
+    
         mv.addObject("value", value);
        return mv;
 
     }
+
+
+    @GetMapping("/category/{value}")
+    public ModelAndView filterByCategory(@PathVariable (name="value") String value, Model model){
+       ModelAndView mv = new ModelAndView("userhomepage");
+       ArrayList<Activity> activities= (ArrayList<Activity>) activityRepository.findAll();
+
+       int index = (int)(Math.random() * activities.size());
+    mv.addObject("randomActivity", activities.get(index));
+
+    ArrayList<ActivityInCategory> activityInCategories = initializeActivityInCategory();
+    if(!value.equals("ALL")){
+        activityInCategories.removeIf(activity->(!activity.getCategory().equals(value)));
+    }
+    User user = (User) model.asMap().get("user");
+    mv.addObject("user", user);      
+    mv.addObject("activities", activityInCategories);
+    
+        mv.addObject("value", value);
+       return mv;
+
+    }
+    */
     
     @GetMapping
     public ModelAndView viewMain(Model model) {
         ModelAndView mv = new ModelAndView("userhomepage");
         User user = (User) model.asMap().get("user");
+        if(user==null){
+            user = new User();
+        }
+
+        mv.addObject("cost", 1000);
+        mv.addObject("category", "ALL");
         mv.addObject("user", user);
         mv.addObject("value", 1000);
         ArrayList<Activity> activities = (ArrayList<Activity>) activityRepository.findAll();
-        mv.addObject("activities", activities);
+
+        mv.addObject("activities", initializeActivityInCategory());
+
         int index = (int)(Math.random() * activities.size());
         mv.addObject("randomActivity", activities.get(index));
         return mv;
     }
     
 
+
+    //Initialize session attributes here:
+    
     @PostMapping
     public ModelAndView processRegister(User user) {
        ModelAndView mv = new ModelAndView("userhomepage");
@@ -87,12 +157,44 @@ public class DashboardController {
             return new ModelAndView("welcome");
         }
         ArrayList<Activity> activities=(ArrayList<Activity>) activityRepository.findAll();
+
+
+        mv.addObject("activities", initializeActivityInCategory());
+        mv.addObject("cost", 1000);
+        mv.addObject("category", "ALL");
        int index = (int)(Math.random() * activities.size());
         mv.addObject("randomActivity", activities.get(index));
         mv.addObject("value", 1000);
-        mv.addObject("activities",activityRepository.findAll());
 
         return mv;
+    }
+
+
+    public ArrayList<ActivityInCategory> initializeActivityInCategory(){
+
+        //Create activityInCategoryList
+        ArrayList<Category> cat = (ArrayList<Category>) categoryRepository.findAll();
+
+        ArrayList<ActivityInCategory> list = new ArrayList<ActivityInCategory>();
+        for(Category c: cat){
+            ArrayList<String> str = (ArrayList<String>)incategoryRepository.FindByCategory_name(c.getName());
+            
+            for(String s: str){
+                Activity a = activityRepository.findByName(s);
+                ActivityInCategory aInCat = new ActivityInCategory();
+                aInCat.setActivityId(a.getId());
+                aInCat.setCategory(c.getName());
+                aInCat.setCategoryId(c.getId());
+                aInCat.setDescription(a.getDescription());
+                aInCat.setCost(a.getCost());
+                aInCat.setName(a.getName());
+
+                list.add(aInCat);
+            }
+        }
+        return list;
+
+
     }
 
  
